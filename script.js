10213990be51ff865760cdb88db11c6be228e8fd
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-11 10:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+const button = document.querySelector("#button");
+const name = document.querySelector("#name");
+const year = document.querySelector("#year");
+const url = document.querySelector("#url");
+button.addEventListener("click", (event) => {
+  if (name.value && !year.value) url.innerHTML += `?name=${name.value}`;
+  else if (!name.value && year.value) url.innerHTML += `?year=${year.value}`;
+  else url.innerHTML += `?name=${name.value}&year=${year.value}`;
+});
